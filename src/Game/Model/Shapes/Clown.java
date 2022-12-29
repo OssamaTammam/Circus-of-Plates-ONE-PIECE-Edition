d56a971ab60b707e4caf432252df399054a506e3
karimelkazaz <es-KarimAshraf2025@alexu.edu.eg>
2022-12-29 21:58:41 +0200

    Check Intersection

--- src/Game/Model/Shapes/Clown.java
+++ src/Game/Model/Shapes/Clown.java
@@ -45,25 +45,26 @@ public class Clown extends ImageObject implements Cloneable {
     public boolean checkIntersectAndAdd(GameObject shape) {
         int midX = shape.getX() + shape.getWidth() / 2;
         int y = shape.getY() + shape.getHeight();
-        // check if the shape is falling on the left stick then add it to stack if it does
         if (left.isEmpty()) {
-            if (stickLeft.getX() <= midX && y >= stickLeft.getY()) {
+            if (stickLeft.getX() <= midX && midX <= (stickLeft.getX() + stickLeft.getWidth() / 2)
+                    && y>=stickLeft.getY()) {
                 shape.setY(stickLeft.getY() - shape.getHeight());
                 return addShape(shape, left);
             }
         } else {
             RemoveFromStk(left);
-            AddToStk(left); // to make sure that the shape is at the top of the stack
+            AddToStk(left);
             GameObject top = left.peekLast();
             if (top != null) {
-                if (top.getX() <= midX &&  y >= top.getY()) {
+                if (top.getX() <= midX && midX <= (top.getX() + top.getWidth()) && y>=top.getY()) {
                     shape.setY(top.getY() - shape.getHeight());
                     return addShape(shape, left);
                 }
             }
         }
         if (right.isEmpty()) {
-            if (stickRight.getX() <= midX  && y >= stickRight.getY()) {
+            if ((stickRight.getX() + stickRight.getWidth() / 2) <= midX
+                    && midX <= (stickRight.getX() + stickRight.getWidth()) && y>=stickRight.getY()) {
                 shape.setY(stickRight.getY() - shape.getHeight());
                 return addShape(shape, right);
             }
@@ -72,7 +73,7 @@ public class Clown extends ImageObject implements Cloneable {
             AddToStk(right);
             GameObject top = right.peekLast();
             if (top != null) {
-                if (top.getX() <= midX && y >= top.getY()) {
+                if (top.getX() <= midX && midX <= (top.getX() + top.getWidth()) && y>=top.getY()) {
                     shape.setY(top.getY() - shape.getHeight());
                     return addShape(shape, right);
                 }
