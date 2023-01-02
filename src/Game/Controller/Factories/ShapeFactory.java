bfee3d16c4101fba933db8e92c42b3e75d6ed8a3
OssamaTammam <ossamatammam@gmail.com>
2023-01-02 19:38:04 +0200

    DONE

--- src/Game/Controller/Factories/ShapeFactory.java
+++ src/Game/Controller/Factories/ShapeFactory.java
@@ -1,24 +1,13 @@
 package Game.Controller.Factories;
 
-//import Game.Controller.Loader.ShapesLoader;
-
 import Game.Model.Shapes.ImageObject;
-import Game.Model.Shapes.Shape;
-import Game.Model.Shapes.ShapeState;
 import Game.Controller.Logging;
 import eg.edu.alexu.csd.oop.game.GameObject;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FilenameFilter;
-import java.lang.reflect.InvocationTargetException;
-import java.util.*;
-import java.util.List;
+
 
 public class ShapeFactory {
-    private List<Class<? extends Game.Model.Shapes.Shape>> loadedClass;
-    private Map<String, BufferedImage> mp; // Flyweight Design Pattern
     public static ShapeFactory instance;
     Logging log = new Logging();
 
@@ -34,14 +23,6 @@ public class ShapeFactory {
         return instance;
     }
 
-    /**
-     * Initializing Factory
-     */
-    private ShapeFactory() {
-        loadedClass = new ArrayList<>();
-        mp = new HashMap<>();
-    }
-
     public GameObject getRandomImage(int posX, int posY, int screenWidth, int screenHeight) {
 
         int imgNum = (int) (Math.random() * 3 + 1);
@@ -51,8 +32,8 @@ public class ShapeFactory {
         return plate;
     }
 
-    public boolean isSame(GameObject a, GameObject b) {  // Compares Images by Resolution and By RGB of every Pixel
-
+    public boolean isSame(GameObject a, GameObject b) {
+        // Compares Images by Resolution and By RGB of every Pixel
         BufferedImage imageA = a.getSpriteImages()[0];
         BufferedImage imageB = b.getSpriteImages()[0];
         if (imageA.getWidth() == imageB.getWidth() && imageA.getHeight() == imageB.getHeight()) {
